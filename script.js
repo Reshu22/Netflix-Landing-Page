f0bb9ed56f84e21b2e5033dfcad4988986b9003c
Reshu22 <95234630+Reshu22@users.noreply.github.com>
2023-03-02 19:50:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// const faqs=document.querySelectorAll(".faq");
+
+// faqs.forEach(faq =>{
+//   faq.addEventListener("click", () => {
+//     faq.classList.toggle("active")
+//   })
+// })
+
+// function show_hide(id) {
+//   var x = document.getElementById(id);
+//   var y=document.getElementsByClassName('answer');
+//   //  y.style.display= "none";
+//   if(x){
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+// else{
+//   y.style.display= "none";
+// }
+// }
